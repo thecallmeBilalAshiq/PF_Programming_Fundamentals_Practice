f0308199a9cfbfd1e4d010751bc4c6b36ec84cb6
Muhammad Bilal <138978969+thecallmeBilalAshiq@users.noreply.github.com>
2024-12-04 00:03:38 +0500

    Create ternary_operator.cpp

---
+++ Day_02/ternary_operator.cpp
@@ -0,0 +1,32 @@
+// --------------------------------------
+// Task - 03
+
+
+// Write a C++ code
+// - Take 2 numbers from user 
+// - x,y
+// if (x>y) ----> print ("x is greater than y")
+// else ----->  print("y is greater than x")
+
+// //////////////////////////////////////////
+// Buttttttttttttttttttt
+
+
+// you are not allowed to use if-else or switch case
+// Socho
+
+
+
+#include <iostream>
+
+int main() 
+{
+    int a = 10, b = 20;
+    
+    // ternary operator
+    int max = (a > b) ? a : b;
+
+    std::cout << "The maximum value is___" << max << std::endl;
+
+    return 0;
+}
