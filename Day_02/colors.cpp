96347ca97b7d2a88f80b6cebea59ad496f0f5d44
Muhammad Bilal <138978969+thecallmeBilalAshiq@users.noreply.github.com>
2024-12-04 00:13:46 +0500

    Update colors.cpp

--- Day_02/colors.cpp
+++ Day_02/colors.cpp
@@ -4,6 +4,19 @@
 
 // include this library   =====>>>>>>>   #include <windows.h> 
 // wrrit this line        =====>>>>>>>   system("color 1B");
+// 1  ----> text color
+// B  ----> background color
+
+
+//                     Color id	Color	Color id	Color
+//                     1	Blue	9	Light Blue
+//                     2	Green	0	Black
+//                     3	Aqua	A	Light Green
+//                     4	Red	B	Light Aqua
+//                     5	Purple	C	Light Red
+//                     6	Yellow	D	Light Purple
+//                     7	White	E	Light Yellow
+//                     8	Gray	F	Bright White
 
 
 #include <iostream>
