577c6b5d623eb8e867a823a9ae026ffb14c697c7
Muhammad Bilal <138978969+thecallmeBilalAshiq@users.noreply.github.com>
2024-12-03 23:57:41 +0500

    Create colors.cpp

---
+++ Day_02/colors.cpp
@@ -0,0 +1,18 @@
+// ============================================================== //
+//                 Colors in C++ console                          //
+// ============================================================== //
+
+
+#include <iostream>
+#include <windows.h> 
+using namespace std;
+
+int main()
+{
+	system("color 1B");        // color
+	
+	cout << "\n\n\n\t\t ---> Muhammad Bilal Ashiq";	
+    cout << "\n\t\t ---> Compiler G \n\n";
+	
+	return 0;
+}
